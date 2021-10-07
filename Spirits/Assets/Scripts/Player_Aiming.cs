7a5012d4d30b3ac828c16cefe2d32aba57b21be8
Michael Diaz <michaeldiaz.compsci@gmail.com>
2021-10-07 15:26:56 -0400

    Animation II
    -Added Front-Facing animation
    -Configured Animator

--- Spirits/Assets/Scripts/Player_Aiming.cs
+++ Spirits/Assets/Scripts/Player_Aiming.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player_Aiming : MonoBehaviour
 {
-    // We'll need this to manipulate the position/rotation/scale
+    // We'll need Lines 7-8 to manipulate the position/rotation/scale and animation triggers
     Rigidbody2D playerBody;
+    Animator playerAnim;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,20 +40,16 @@ public class Player_Aiming : MonoBehaviour
         // Based on the angle/position of the cursor, we'll alter the sprite's direction to make it seem like the player is facing that way
         if(absAngle <= 45)
             // General Direction is UP
-            angle = angle;
+            playerAnim.SetInteger("Direction", 0);
         else if(absAngle > 45 && absAngle <= 135)
         {
+            // General Direction is LEFT or RIGHT
             transform.localScale = new Vector2(distance.x / absDistance, 1);
-            if (distance.x / absDistance > 0)
-                // General Direction is RIGHT
-                angle = angle;
-            else
-                // General Direction is LEFT
-                angle = angle;
+            playerAnim.SetInteger("Direction", 1);
         }
         else
             // General Direction is DOWN
-            angle = angle;
+            playerAnim.SetInteger("Direction", 2);
         
     }
 }
