753e5a1a2f65c29efc9fbc2e31728c1d6282f299
Federico Baron <federicoagustinbaron@gmail.com>
2021-10-10 17:33:56 -0400

    add ghost capturing

--- Spirits/Assets/Scripts/Enemy.cs
+++ Spirits/Assets/Scripts/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
     
 	public int maxHealth = 100;
 	int currentHealth;
+    bool isFainted = false;
+    int ghostType = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,20 +30,51 @@ public class Enemy : MonoBehaviour
     	}
     }
 
-    public void isFainted(){
+    public bool IsFainted(){
+        return isFainted;
+    }
+
+    void Faint(){
+        isFainted = true;
+        Debug.Log("we faint");
+        StartCoroutine(StartFaintTimer());
+    }
+
+    public IEnumerator StartFaintTimer(){
+        int minimum = 2;
+        int maximum = 5;
+
+        float timeRemaining = Random.Range(minimum, maximum);
         
+        while(timeRemaining > 0){
+            Debug.Log("we at: " + timeRemaining);
+            yield return new WaitForSeconds(1.0f);
+            timeRemaining--;
+        }
+        isFainted = false;
+        Debug.Log("unfaint");
+    }
+
+    public void Captured(){
+        Destroy(gameObject);
+    }
+
+    public int GetGhostType(){
+        return ghostType;
     }
 
     void Die()
     {
     	Debug.Log("Enemy Died");
+
+        Faint();
     	// Die animation
         // animator.SetBool("IsDead", true);
 
     	// Disable enemy
         // Destroy(gameObject);
-        GetComponent<Collider2D>().enabled = false;
-        this.enabled = false;
+        // GetComponent<Collider2D>().enabled = false;
+        // this.enabled = false;
         
     }
 }
