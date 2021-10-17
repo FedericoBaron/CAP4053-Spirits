f4481e1240b26bae6db6234b30494db8467c8c4e
Federico Baron <federicoagustinbaron@gmail.com>
2021-10-17 15:56:21 -0400

    add money text

---
+++ Spirits/Assets/MoneyTextManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyTextManager : MonoBehaviour
+{
+    public static MoneyTextManager instance;
+
+    public Text moneyText;
+    public int money;
+
+    private void Awake(){
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyText.text = "Money: 0";
+        money = 0;
+    }
+
+    public void setText(int amount){
+        Debug.Log("SET TEXT");
+        money += amount;
+        moneyText.text = "Money: " + money;
+    }
+}
