98815518d227144881370f3178f2ea04929dbca9
Ellie <elliekozlowski7000@gmail.com>
2021-10-20 12:56:38 -0400

    update health bar color

--- Spirits/Assets/Scripts/HealthBar.cs
+++ Spirits/Assets/Scripts/HealthBar.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HealthBar : MonoBehaviour
 {
@@ -8,6 +9,21 @@ public class HealthBar : MonoBehaviour
 
     public void UpdateHealthBar() 
     {
-        healthBarImage.fillAmount = Mathf.Clamp(player.GetComponent<Player_Combat>().health / player.GetComponent<Player_Combat>().maxHealth, 0, 1f);
+        float playerHealth = player.GetComponent<Player_Combat>().health;
+        float playerMaxHealth = player.GetComponent<Player_Combat>().maxHealth;
+
+        float duration = 0.75f * (playerHealth / playerMaxHealth);
+        healthBarImage.fillAmount = Mathf.Clamp(playerHealth / playerMaxHealth, 0, 1f);
+
+        Color newColor = Color.green;
+        if (playerHealth < playerMaxHealth * 0.25f) 
+        {
+            newColor = Color.red;
+        } 
+        else if (playerHealth < playerMaxHealth * 0.66f) 
+        {
+            newColor = new Color(1f, .64f, 0f, 1f);
+        }
+        healthBarImage.DOColor(newColor, duration);
     }
 }
