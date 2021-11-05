675e1652509c1c96286cd003a9c0047f5192f225
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-05 12:17:54 -0400

    test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CAP4053-Spirits
 this is a test
+this is another test
+this
---
+++ Spirits/Assets/RecipeItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeItem : MonoBehaviour
+{
+    string name;
+    int type;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
